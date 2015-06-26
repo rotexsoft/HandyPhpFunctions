9b1f451b3ccc6430e4363dfd8697ae49c1149df3
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2015-06-25 21:31:24 -0600

    Update array-functions.php

--- src/array-functions.php
+++ src/array-functions.php
@@ -174,6 +174,28 @@ function array_get(array $array, $key, $default_value=null) {
 	}
 }
 
+/**
+ * 
+ *  based on http://stackoverflow.com/questions/1019076/how-to-search-by-key-value-in-a-multidimensional-array-in-php
+ * 
+ */
+function search_r($array, $key, $value, &$results) {
+	
+    if (!is_array($array)) {
+        return;
+    }
+
+    if ( array_key_exists($key, $array) && $array[$key] === $value) {
+    
+        $results[] = $array;
+    }
+
+    foreach ($array as $subarray) {
+    
+        search_r($subarray, $key, $value, $results);
+    }
+}
+
 /*
 $new = array();
 recursively_copy_array($array['where'], $new);
